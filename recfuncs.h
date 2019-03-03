a15e1af5299e1f7032faaff917a08080f00ad774
rieserfelix <dafelix.rieser@gmail.com>
2019-03-03 11:21:55 +0100

    first

--- recfuncs.h
+++ recfuncs.h
@@ -1,6 +1,20 @@
 #ifndef ___RECURSIVE_POTPOURRI
 #define ___RECURSIVE_POTPOURRI
 
+struct CalculationResults {
+  int no_of_nums;
+  int no_of_sums;
+  int* sums;
+  int* binary_sums;
+  int* num_factorials;
+};
 
+int calc_array_size(int n);
+int convert_to_binary(int n);
+int* create_array(int size);
+int calc_factorial(int n);
+void calc_sums(int* nums,int size, int* out_arr, int* out_arr_bin);
+CalculationResults perform_calculations(int* nums, int n);
+int thx(int* nums,int size, int* out_arr, int* out_arr_bin, int i, int index, int sum);
 
 #endif
